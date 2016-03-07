d8379079a96124d26678b98e1cd016ee893e7257
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-03-07 11:59:50 -0700

    Changed routes to movies.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 var express = require('express'); // connects Express
 var app = express(); // create Express application object
 var bodyParser = require('body-parser'); // connects body-parser
-var routes = require('./routes/routes.js'); // connects routes.js
+var movies = require('./routes/routes.js'); // connects routes.js
 
 // Middleware stack
 app.use(bodyParser.urlencoded({ extended: true })); // enables reading the body of a POST request
 app.use(bodyParser.json()); // enables reading a JSON object in the body of POST request
-app.use('/', routes); // middleware to serve routes
+app.use('/', movies); // middleware to serve routes
 
 // Server
 app.listen(3000); // starts server
