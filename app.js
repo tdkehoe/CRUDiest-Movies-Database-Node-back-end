23d6fc2ed435f761ca141839c8b2a556681cbb22
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-03-08 21:20:27 -0700

    Put movies into URL.

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ require('dotenv').load();
 var bodyParser = require('body-parser'); // connects body-parser
 var movies = require('./routes/routes.js'); // connects routes.js
 
+var cors = require('cors');
+app.use(cors());
+
 // Middleware stack
 app.use(bodyParser.urlencoded({ extended: true })); // enables reading the body of a POST request
 app.use(bodyParser.json()); // enables reading a JSON object in the body of POST request
-app.use('/', movies); // middleware to serve routes
+app.use('/movies', movies); // middleware to serve routes
 
 // Server
 app.listen(process.env.PORT || 3000); // starts server
